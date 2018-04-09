afa3a495e3eb8403f9dab5aa9f0bed5edbf58322
Grzegorz Gil <grzechgil@poczta.onet.pl>
2018-04-09 18:46:09 +0200

    ManagerCourse

--- src/java/sda/java9/wpj/advanced/oop/course/people/CourseManager.java
+++ src/java/sda/java9/wpj/advanced/oop/course/people/CourseManager.java
@@ -1,5 +1,38 @@
 package sda.java9.wpj.advanced.oop.course.people;
 
 public class CourseManager {
-    
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setFistName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    public void setPhoneNumber(String email) {
+		this.phoneNumber = phoneNumber;
+	}
 }
