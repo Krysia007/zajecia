7753f3e2b474e369779fd758014870dceadfb215
SJMalecki <sebastian.jacek.malecki@gmail.com>
2018-04-09 18:47:56 +0200

    Board1

--- src/java/sda/java9/wpj/advanced/oop/course/items/Board.java
+++ src/java/sda/java9/wpj/advanced/oop/course/items/Board.java
@@ -2,4 +2,51 @@ package sda.java9.wpj.advanced.oop.course.items;
 
 public class Board {
     
+	private String clean;
+	private String sponge;
+	private String freeSpace;
+	private String boardColor;
+
+	
+	public String getClean(){
+		return clean;
+	}
+	
+	public String getSponge(){
+		return sponge;
+	}
+	
+	public String getFreeSpace(){
+		return freeSpace;
+	}
+	
+	public String getBoardColor(){
+		return boardColor;
+	}
+	
+	public void setClean(String clean){
+		this.clean = clean;
+	}
+	
+	public void setSponge(String sponge){
+		this.sponge = sponge;
+	}
+	
+	public void setFreeSpace(String freeSpace){
+		this.freeSpace = freeSpace;
+	}
+	
+	public void setBoardColor(String boardColor){
+		this.boardColor = boardColor;
+	}
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
