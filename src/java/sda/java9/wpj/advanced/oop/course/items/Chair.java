8dbdf56c3dfa2abd14d2b259eeecdb6412cdabad
Jacek Ziober <jacekziober1@gmail.com>
2018-04-09 19:01:04 +0200

    class Chair

--- src/java/sda/java9/wpj/advanced/oop/course/items/Chair.java
+++ src/java/sda/java9/wpj/advanced/oop/course/items/Chair.java
@@ -2,26 +2,17 @@ package sda.java9.wpj.advanced.oop.course.items;
 
 public class Chair {
 
-	private String fabric; 
 	private String color;
 	private int numberOfLegs;			
 	private boolean backrestOfTheChair;	//oparcie
-	private String mark;				//marka
-	private boolean swivel;				//obrotowe
 	private boolean useOfChair;			//czy krzesło jest użyte
+	
 	
 	public boolean getUseOfChair(){
 		return useOfChair;
 	}
 	public void setUseOfChair(boolean useOfChair){
 		this.useOfChair = useOfChair;
-	}
-	
-	public String getFabric(){
-		return fabric;
-	}
-	public void setFabric(String fabric){
-		this.fabric = fabric;
 	}
 	
 	public void setColor(String color){
@@ -46,21 +37,8 @@ public class Chair {
 		this.backrestOfTheChair = backrestOfTheChair;
 	}
 	
-	public String getMark(){
-		return mark;
-	}
-	public void setMark(String mark){
-		this.mark = mark;
-	}
 	
-	public boolean getSwivel(){
-		return swivel;
-	}
-	
-	public void setSwivel(){
-		this.swivel = swivel;
-	}
-	
+
 	
 	
 	
