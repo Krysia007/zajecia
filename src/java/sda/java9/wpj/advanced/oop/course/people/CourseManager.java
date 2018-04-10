310a23aa5aa3ef34a745f05cf500e15bcdc1693d
honestit <michal.kupisinski@gmail.com>
2018-04-10 05:45:16 +0200

    Nowa metoda sayGreetings() menedżera kursu wraz z drukowaniem wizytówki

--- src/java/sda/java9/wpj/advanced/oop/course/people/CourseManager.java
+++ src/java/sda/java9/wpj/advanced/oop/course/people/CourseManager.java
@@ -12,7 +12,40 @@ public class CourseManager {
     private ClassRoom classRoom;
     
     public String sayGreetings() {
-        return "Witajcie na kursie Java";
+        String greet = "Witajcie na kursie Java";
+        greet += "\nNazywam sie " + this.firstName + " " + this.lastName;
+        greet += "\nJestem opiekunem tego kursu i w razie wszelkich pytan jestem";
+        greet += " do Waszej dyspozycji. Nie mowie polskimi znakami, bo wiem, ze sa z tym problemy...";
+        greet += "\nJezeli potrzebowalibyscie kontaktu do mnie, to oto moja wizytowka:\n\n";
+        String businessCard = getBusinessCard();
+        greet += businessCard;
+        return greet;
+    }
+    
+    public String getBusinessCard() {
+        String title = "Mr. " + this.firstName + " " + this.lastName;
+        String phone = "Tel:   " + this.phoneNumber;
+        String email = "Email: " + this.email;
+        
+        int longestData = (int) Math.max(title.length(), Math.max(phone.length(), email.length()));
+        
+        String header = "--" + signLine(longestData, '-') + "--";
+        String footer = "--" + signLine(longestData, '-') + "--";
+        String breakLine = "| " + signLine(longestData, ' ') + " |";
+        
+        title = "| " + title + signLine(longestData - title.length(), ' ') + " |";
+        phone = "| " + phone + signLine(longestData - phone.length(), ' ') + " |";
+        email = "| " + email + signLine(longestData - email.length(), ' ') + " |";
+        
+        return (header.concat("\n").concat(title).concat("\n")
+                    .concat(breakLine).concat("\n").concat(phone).concat("\n")
+                    .concat(email).concat("\n").concat(footer));
+    }
+    
+    private String signLine(int length, char sign) {
+        char[] chars = new char[length];
+        java.util.Arrays.fill(chars, sign);
+        return new String(chars);
     }
     
     public void openRoom(ClassRoom room) {
